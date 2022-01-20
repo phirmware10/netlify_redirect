3355c4076a5e25f6dcf93d9721bcfb73dfa43ad2
phirmware <chibuzor.ojukwu@gmail.com>
2022-01-21 00:41:11 +0100

    feat: dato fetch

--- plugin/redirect-plugin/index.js
+++ plugin/redirect-plugin/index.js
@@ -1,10 +1,47 @@
+require('dotenv').config()
+const superagent = require('superagent')
+const url = 'https://graphql.datocms.com/'
+
+const datoFetch = async (query, variables, config) => {
+  const { token } = config
+  const result = await superagent
+    .post(url)
+    .send({ query, ...(variables && { variables }) })
+    .set('Content-Type', 'application/json')
+    .set('authorization', `Bearer ${token}`)
+
+  return result.body.data
+}
+
 module.exports = {
-  onPreBuild: ({ netlifyConfig }) => {
-    netlifyConfig.redirects.push({
-      from: '/',
-      to: 'https://outlier.org',
-      status: 302
+  onPreBuild: async ({ netlifyConfig }) => {
+    const { build: { environment } } = netlifyConfig
+
+    const config ={ token: environment.token || process.env.DATO_TOKEN }
+    const { allCourseDownloads } = await fetchfiles(config)
+    allCourseDownloads.length && allCourseDownloads.forEach(courseDownload => {
+      const { slug, file: { url } } = courseDownload
+      netlifyConfig.redirects.push({
+        from: `/${slug}`,
+        to: url,
+        status: 302
+      })
     })
     console.log(netlifyConfig, 'netlify config')
   }
+}
+
+function fetchfiles (config) {
+  const query = `{
+    allCourseDownloads {
+      id
+      file {
+        id
+        url
+      }
+      slug
+    }
+  }`
+
+  return datoFetch(query, {}, config)
 }
