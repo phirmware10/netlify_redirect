101c608612c4a11d321d582297675c8705c78380
phirmware <chibuzor.ojukwu@gmail.com>
2022-01-21 17:12:28 +0100

    feat: test render build

--- build.js
+++ build.js
@@ -1,1 +1,42 @@
-console.log(process.env.DATO_TOKEN)
+const token = process.env.DATO_TOKEN || '61a51e9e1c16f4e972ea78180c2033'
+const url = 'https://graphql.datocms.com/'
+const superagent = require('superagent')
+const fs = require('fs')
+
+const datoFetch = async (query, variables) => {
+  const result = await superagent
+    .post(url)
+    .send({ query, ...(variables && { variables }) })
+    .set('Content-Type', 'application/json')
+    .set('authorization', `Bearer ${token}`)
+
+  return result.body.data
+}
+
+function fetchfiles (config) {
+  const query = `{
+    allCourseDownloads {
+      id
+      file {
+        id
+        url
+      }
+      slug
+    }
+  }`
+
+  return datoFetch(query, {}, config)
+}
+
+async function createRedirects () {
+  const { allCourseDownloads } = await fetchfiles()
+  console.log(allCourseDownloads, 'all course downloads')
+  let data = ''
+  allCourseDownloads.length && allCourseDownloads.forEach(courseDownload => {
+    const { slug, file: { url } } = courseDownload
+    data += `/${slug} ${url} 302\n`
+  })
+  fs.writeFileSync('./_redirects', data)
+}
+
+createRedirects()
